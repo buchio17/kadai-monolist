7d3277926ef73c3645b176762a9c7586fa3e7162
buchio17 <buchio17@gmail.com>
2018-02-20 03:41:41 +0000

    user registration

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(params_user)
+    
+    if @user.save
+      flash[:success] = 'ユーザーを登録しました。'
+      redirect_to @user
+    else
+      flash.now[:danger] = 'ユーザーの登録に失敗しました。'
+      render :new
+    end
+  end
+  
+  private
+  
+  def params_user
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
